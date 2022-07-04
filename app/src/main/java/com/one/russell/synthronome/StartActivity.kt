e0e5dcb589bf09bce8ecfde5c4ba2adf2c35ecaa
Alexey Tarasko <rassel2121@gmail.com>
2022-07-04 13:12:53 +0700

    Add splash screen

--- app/src/main/java/com/one/russell/synthronome/StartActivity.kt
+++ app/src/main/java/com/one/russell/synthronome/StartActivity.kt
@@ -2,6 +2,7 @@ package com.one.russell.synthronome
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class StartActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class StartActivity : AppCompatActivity() {
     private val viewModel: StartViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel.restoreValues()
